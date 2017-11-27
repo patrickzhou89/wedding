6292bbede6189adfd9e1a83351a219d96b21314b
unknown <patrick.zhou89@gmail.com>
2017-11-26 17:24:43 -0800

    Replaced picture, cleared typo

--- js/wedding.js
+++ js/wedding.js
@@ -38,7 +38,7 @@
 	},100));
 
 	/* Photo Gallery Navigator */
-	var index=1,galleryLength=11;
+	var index=1,galleryLength=10;
 	function setGalleryImage(index){
 		$('#gallery-image').attr('src',"./styles/images/gallery/"+index+".jpg");
 		$('#navigator > ul').children('.filled').removeClass('filled');
