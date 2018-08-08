dcc9b3777e7a93b7376447aefeddd7800025d356
pdpscarface <patrick.zhou89@gmail.com>
2018-08-07 21:23:37 -0400

    adding session based login plus react stub

---
+++ app/js/admin.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const e = React.createElement;
+
+
+
+
+const domContainer = document.querySelector('#main');
+ReactDOM.render(e(LikeButton), domContainer);
--- devserver.js
+++ devserver.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 var db, hostFolder,port;
+const session = require('express-session');
+
 if(process.argv){
 	let environment=process.argv[2];
 	console.log(environment);
@@ -15,7 +18,7 @@ if(process.argv){
 	else if(environment=="production"){
 		db='wedding';
 	    hostFolder="dist";
-	    port=3003;
+	    port=3002;
 	}
 }
 console.log('using database'+db+' and folder:' +hostFolder);
@@ -61,6 +64,7 @@ var guestMadLibResponse = new Schema({
 var guestMadLibResponseModel=mongoose.model('guestMadLibResponse', guestMadLibResponse,'guestMadLibResponse');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.use(express.static(hostFolder));
 app.get('/',(req, res) => res.sendFile('index.html',{root:__dirname+"/"+hostFolder}));
 
@@ -71,11 +75,8 @@ app.post('/rsvp',(req,res)=> {
 		firstName=rsvpNameSearch.firstName,
 		lastName=rsvpNameSearch.lastName;
 	if(firstName && lastName){
-	    //guestReservationModel.findOne({'guestList.firstName':{'$regex':firstName,$options:'i'},'guestList.lastName':{'$regex':lastName,$options:'i'}}).exec(function(err, docs){
-	    firstName=firstName.charAt(0).toUpperCase() + firstName.substr(1);
-	    lastName=lastName.charAt(0).toUpperCase() + lastName.substr(1);
-	    guestReservationModel.findOne({'guestList.firstName':firstName,'guestList.lastName':lastName}).exec(function(err, docs){
-		if (err){
+		guestReservationModel.findOne({'guestList.firstName':{'$regex':firstName,$options:'i'},'guestList.lastName':{'$regex':lastName,$options:'i'}}).exec(function(err, docs){
+			if (err){
 				res.sendStatus(400);
 			}else{
 				if(docs==null){
@@ -87,7 +88,7 @@ app.post('/rsvp',(req,res)=> {
 				}
 			}
 		})
-	}else{
+	}else{o
 		res.sendStatus(404);
 	}
 });
@@ -122,4 +123,84 @@ app.post('/rsvp/madlib', (req, res)=>{
 		});
 	}
 });
+
+app.use(session({
+	key:'user_sid',
+	secret: 'abcd',
+	resave:false,
+	saveUninitialized:false,
+	cookie:{
+		expires: 60000
+	}
+}));
+
+app.use((req,res,next)=>{
+	if(req.cookies.user_sid && !req.session.user){
+		res.clearCookie('user_sid');
+	}
+	next();
+});
+
+var sessionChecker = (req, res, next)=> {
+	if(req.session.user && req.cookies.user_sid){
+		res.redirect('/admin');
+	}else{
+		next();
+	}
+};
+
+app.route('/login').get(sessionChecker, (req, res)=>{
+	res.sendFile('login.html',{root:__dirname+"/"+hostFolder})
+}).post((req,res)=>{
+	var username = req.body.username,
+	password = req.body.password;
+	console.log(username, password);
+	req.session.user={user:username};
+	res.redirect('/admin');
+});
+
+app.get('/admin', (req, res) => {
+	console.log(req.session.user)
+	console.log(req.cookies.user_sid);
+	if(req.session.user && req.cookies.user_sid){
+		res.sendFile('admin.html',{root:__dirname+"/"+hostFolder})
+	}else{
+		res.redirect('/login');
+	}
+});
+
+app.get('/logout', (req,res)=>{
+	if (req.session.user && req.cookies.user_sid) {
+        res.clearCookie('user_sid');
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+})
+
+app.post('/admin/guestList',sessionChecker,(req,res)=>{
+	let guestReservationMap = {};
+	guestReservationModel.find({},function(err,guestReservationList){
+		for(let guestReservation of guestReservationList){
+			let guestReservationId = guestReservation['_id'];
+			console.log(guestReservationId);
+			if(guestReservationMap[guestReservationId]==undefined){
+				guestReservationMap[guestReservationId]={
+					guestList:guestReservation.guestList
+				}
+			}
+		}		
+		guestResponseModel.find({},function(err, guestResponseList){
+			for(let guestResponse of guestResponseList){
+				let temp=guestReservationMap[guestResponse.guestPartyId];
+				if(temp){
+					delete temp.guestPartyId;
+					delete temp['_id'];
+					Object.assign(temp,guestResponse.toObject());
+				}
+			}
+			res.json(guestReservationMap);
+		})
+	});
+})
 app.listen(port, () => console.log('Listening on port '+port));
